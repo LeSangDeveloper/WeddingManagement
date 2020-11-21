68366bf6484780939c76d536c34106069ba825a5
LeeShangCyber <lesang541191309@gmail.com>
2020-11-21 22:28:17 +0700

    Add AbstractModel

---
+++ Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/AbstractModel.java
@@ -0,0 +1,25 @@
+package Module.WeddingManagement.ApplicationModel;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+public class AbstractModel implements Serializable {
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this, false);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj, false);
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+}
