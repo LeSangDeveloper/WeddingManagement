29967dcdaa2c8b43248d5a0b682a27e9c7950d6b
LeeShangCyber <lesang541191309@gmail.com>
2020-11-21 23:07:04 +0700

    initialize

--- Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/Booking.java
+++ Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/Booking.java
@@ -15,6 +15,87 @@ public class Booking extends AbstractModel {
     private String note;
     private double price;
 
+    public Booking(int hallId, int menuId, int employeeId, int customerId, Date createdAt, Date dateOfPayment, int tables, ShiftType shift, String note, double price) {
+        this.hallId = hallId;
+        this.menuId = menuId;
+        this.employeeId = employeeId;
+        this.customerId = customerId;
+        this.createdAt = createdAt;
+        this.dateOfPayment = dateOfPayment;
+        this.tables = tables;
+        this.shift = shift;
+        this.note = note;
+        this.price = price;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getDateOfPayment() {
+        return dateOfPayment;
+    }
+
+    public void setDateOfPayment(Date dateOfPayment) {
+        this.dateOfPayment = dateOfPayment;
+    }
+
+    public int getTables() {
+        return tables;
+    }
+
+    public void setTables(int tables) {
+        this.tables = tables;
+    }
+
+    public ShiftType getShift() {
+        return shift;
+    }
+
+    public void setShift(ShiftType shift) {
+        this.shift = shift;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public int getHallId() {
+        return hallId;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
     public enum ShiftType
     {
         Morning,
--- Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/BookingService.java
+++ Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/BookingService.java
@@ -1,4 +1,19 @@
 package Module.WeddingManagement.ApplicationModel;
 
 public class BookingService extends  AbstractModel {
+    private int bookingId;
+    private int serviceId;
+
+    public BookingService(int bookingId, int serviceId) {
+        this.bookingId = bookingId;
+        this.serviceId = serviceId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
 }
