c523a6249c495b102f5fc76bdfdae1f3d2742894
LeeShangCyber <lesang541191309@gmail.com>
2020-11-30 09:59:19 +0700

    + add List Foods

--- Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/Menu.java
+++ Module.WeddingManagement.ApplicationModel/src/main/java/Module/WeddingManagement/ApplicationModel/Menu.java
@@ -1,13 +1,20 @@
 package Module.WeddingManagement.ApplicationModel;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Menu extends AbstractModel {
     private int menuId;
     private Date createdAt;
+    private List<MenuFood> foods = new ArrayList<>();
 
     public Menu(Date createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public List<MenuFood> getFoods() {
+        return foods;
     }
 
     public int getMenuId() {
