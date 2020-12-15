60dc6259bed30bba7d316ba1f1b0afd2dbba7b99
sang <lesang541191309@gmail.com>
2020-12-16 00:15:48 +0700

    git report again

--- Module.WeddingManagement.UseCase/src/main/java/Module/WeddingManagement/UseCase/GetReport.java
+++ Module.WeddingManagement.UseCase/src/main/java/Module/WeddingManagement/UseCase/GetReport.java
@@ -6,10 +6,8 @@ import Module.WeddingManagement.Repository.DBContext;
 import com.sun.org.apache.xpath.internal.operations.Or;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.math.MathContext;
+import java.util.*;
 
 public class GetReport implements IListOuput<Order> {
 
@@ -29,6 +27,53 @@ public class GetReport implements IListOuput<Order> {
         this.year = year;
     }
 
+    private boolean isEqualWithoutTime(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal1.set(Calendar.HOUR_OF_DAY, 0);
+        cal1.set(Calendar.MINUTE, 0);
+        cal1.set(Calendar.SECOND, 0);
+        cal1.set(Calendar.MILLISECOND, 0);
+        int day1 = cal1.get(Calendar.DAY_OF_MONTH);
+        int month1 = cal1.get(Calendar.MONTH);
+        int year1 = cal1.get(Calendar.YEAR);
+
+        cal1.setTime(date2);
+        cal1.set(Calendar.HOUR_OF_DAY, 0);
+        cal1.set(Calendar.MINUTE, 0);
+        cal1.set(Calendar.SECOND, 0);
+        cal1.set(Calendar.MILLISECOND, 0);
+        int day2 = cal1.get(Calendar.DAY_OF_MONTH);
+        int month2 = cal1.get(Calendar.MONTH);
+        int year2 = cal1.get(Calendar.YEAR);
+
+        return (day1 == day2) && (month1 == month2) && (year1 == year2);
+    }
+
+    private boolean isEqualWithoutDayTime(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance(TimeZone.getDefault());
+        cal1.setTime(date1);
+        cal1.set(Calendar.DAY_OF_MONTH, 0);
+        cal1.set(Calendar.HOUR_OF_DAY, 0);
+        cal1.set(Calendar.MINUTE, 0);
+        cal1.set(Calendar.SECOND, 0);
+        cal1.set(Calendar.MILLISECOND, 0);
+        cal1.setTimeZone(TimeZone.getDefault());
+        int month1 = cal1.get(Calendar.MONTH);
+        int year1 = cal1.get(Calendar.YEAR);
+
+        cal1.setTime(date2);
+        cal1.set(Calendar.DAY_OF_MONTH, 0);
+        cal1.set(Calendar.HOUR_OF_DAY, 0);
+        cal1.set(Calendar.MINUTE, 0);
+        cal1.set(Calendar.SECOND, 0);
+        cal1.set(Calendar.MILLISECOND, 0);
+        int month2 = cal1.get(Calendar.MONTH);
+        int year2 = cal1.get(Calendar.YEAR);
+
+        return (month1 == month2) && (year1 == year2);
+    }
+
     @Override
     public List<Order> GetList() {
         return DBContext.getOrders().FindAll();
@@ -36,9 +81,44 @@ public class GetReport implements IListOuput<Order> {
 
     public List<BigDecimal> GetReportByQuater(){
         List<BigDecimal> result = new ArrayList<>();
+        int minMonth = 0;
+        int maxMonth = 0;
+        switch (quarter) {
+            case 1 :
+                minMonth = 1;
+                maxMonth = 3;
+                break;
+            case 2:
+                minMonth = 4;
+                maxMonth = 6;
+                break;
+            case 3:
+                minMonth = 7;
+                maxMonth = 9;
+                break;
+            case 4:
+                minMonth = 10;
+                maxMonth = 12;
+                break;
+            default:
+                minMonth = 0;
+                maxMonth = 0;
+                break;
+        }
 
-        for (Order order : orders) {
+        for (int i = minMonth; i <= maxMonth; ++i) {
 
+            Date temp = new GregorianCalendar(year, i - 1, 1).getTime();
+            BigDecimal total = new BigDecimal(0);
+
+            for (Order order : orders) {
+                if (isEqualWithoutTime(order.getCreatedAt(), temp)) {
+                    BigDecimal test1 = order.getCash();
+                    total = total.add(test1, MathContext.DECIMAL64);
+                }
+            }
+
+            result.add(total);
         }
 
         return result;
@@ -59,10 +139,17 @@ public class GetReport implements IListOuput<Order> {
         }
         for (int i = 1; i <= maxDate; ++i) {
             Date temp = new GregorianCalendar(year, month - 1, i).getTime();
+            BigDecimal total = new BigDecimal(0);
 
             for (Order order : orders) {
-
+                if (isEqualWithoutTime(order.getCreatedAt(), temp)) {
+                    BigDecimal test1 = order.getCash();
+                    total = total.add(test1, MathContext.DECIMAL64);
+                }
             }
+
+            result.add(total);
+
         }
 
         return result;
@@ -70,6 +157,23 @@ public class GetReport implements IListOuput<Order> {
 
     public List<BigDecimal> GetReportByYear() {
         List<BigDecimal> result = new ArrayList<>();
+        int maxMonth = 12;
+        for (int i = 1; i <= maxMonth; ++i) {
+            Date temp = new GregorianCalendar(year, i - 1, 1).getTime();
+            BigDecimal total = new BigDecimal(0);
+
+            for (Order order : orders) {
+                Date tempDate = order.getCreatedAt();
+                boolean test = isEqualWithoutDayTime(temp, tempDate);
+                if (test) {
+                    BigDecimal test1 = order.getCash();
+                    total = total.add(test1, MathContext.DECIMAL64);
+                }
+            }
+
+            result.add(total);
+
+        }
 
         return result;
     }
--- Module.WeddingManagement.UseCase/src/main/java/Module/WeddingManagement/UseCase/Test.java
+++ Module.WeddingManagement.UseCase/src/main/java/Module/WeddingManagement/UseCase/Test.java
@@ -1,2 +1,44 @@
+package Module.WeddingManagement.UseCase;
+
+import Module.WeddingManagement.ApplicationModel.Booking;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 public class Test {
+    public static void main(String[] args) {
+        ListBooking listBooking = new ListBooking();
+        List<Booking> bookings = listBooking.GetList();
+        System.out.println(bookings);
+
+        GetReport getReport = new GetReport();
+        getReport.setMonth(2);
+        getReport.setYear(2012);
+        getReport.setQuarter(2);
+        List<BigDecimal> cashes = getReport.GetReportByQuater();
+        System.out.println(cashes);
+
+    }
+
+    public static boolean isEqualWithoutTimeTest(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal1.set(Calendar.HOUR_OF_DAY, 0);
+        cal1.set(Calendar.MINUTE, 0);
+        cal1.set(Calendar.SECOND, 0);
+        cal1.set(Calendar.MILLISECOND, 0);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        cal2.set(Calendar.HOUR_OF_DAY, 0);
+        cal2.set(Calendar.MINUTE, 0);
+        cal2.set(Calendar.SECOND, 0);
+        cal2.set(Calendar.MILLISECOND, 0);
+
+        return !(cal1.before(cal2) || cal1.after(cal2));
+    }
+
 }
